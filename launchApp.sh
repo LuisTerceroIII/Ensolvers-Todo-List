b7495293b7a9fb4232bcafe084674e3f0487b963
LuisTerceroIII <luis.espinoza.nav@live.com>
2021-10-16 21:25:06 -0300

    complete functionalities

--- launchApp.sh
+++ launchApp.sh
@@ -2,7 +2,7 @@
 
 #El archivo no debe moverse de directorio o el script no funcionara.
 cd target
-java -jar ensolvers-0.0.1-SNAPSHOT.jar
+java -jar ensolvers-0.0.1-SNAPSHOT.jar &
 cd ..
-cd ensolver_exam_view
+cd ensolver_exam_view &
 npm start
